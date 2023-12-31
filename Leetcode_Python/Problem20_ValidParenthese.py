d60df9ebb007c940d2ce2685d6650c7230923369
shimame <adcshun143@gmail.com>
2023-12-31 22:35:18 +0900

    CheckPairParentheses

--- Leetcode_Python/Problem20_ValidParenthese.py
+++ Leetcode_Python/Problem20_ValidParenthese.py
@@ -1,8 +1,16 @@
-s = "()"
-words = list(s)
-answer = ""
-i = 0
+def common_search(s):
+    pair = {'(':')', '{':'}','[':']'}
+    stack = []
 
-def common_search(self, s):
-    while answer != False:
-        return 
+    if (s[0] not in pair) or (s[-1] in pair):
+        return False
+    
+    for i in s:
+        if i in pair:
+            stack.append(i)
+        elif len(stack) == 0 or pair[stack.pop()] != i:
+            return False
+    return len(stack) == 0
+
+s = "([]"
+print(common_search(s))
