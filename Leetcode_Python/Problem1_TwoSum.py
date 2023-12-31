68f79629251b5a294fce3625329d1ed72aadc439
shimame <adcshun143@gmail.com>
2023-12-31 22:24:14 +0900

    TwoSumEqual9

--- Leetcode_Python/Problem1_TwoSum.py
+++ Leetcode_Python/Problem1_TwoSum.py
@@ -5,11 +5,4 @@ for i, num1 in enumerate(nums):
     for j, num2 in enumerate(nums):
         answer = num1 + num2
         if(target == answer and i < j):
-            print(str(i) + "," + str(j))
-            
-            '''
-            :type nums: List[int]
-            :type target: int
-            :rtype: List[int]
-            '''
-        
+            print(str(i) + "," + str(j))        
