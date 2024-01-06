308d23bc320f67cbe7988cb9cee5b08fa78dfcdc
shimame <adcshun143@gmail.com>
2024-01-06 22:43:10 +0900

    MergeTwoNumsLists

--- Leetcode_Python/Problem88_MergeSortedArray.py
+++ Leetcode_Python/Problem88_MergeSortedArray.py
@@ -1,22 +1,7 @@
 def merge(nums1, m, nums2, n):
-    """
-    :type nums1: List[int]
-    :type m: int
-    :type nums2: List[int]
-    :type n: int
-    :rtype: None Do not return anything, modify nums1 in-place instead.
-    """
-    for i in range(len(nums1) - m):
-       del nums1[-1]
     for i in range(n):
-        for j in range(len(nums1)):
-            if nums1[j] > nums2[i]:
-                nums1.insert(j, nums2[i])
-                print(f"j = {j},   nums2[{i}] = {nums2[i]}")
-            elif j == len(nums1) - 1:
-                nums1.append(nums2[i])
-    if len(nums1) == 0:
-        nums1 += nums2
+        nums1[i + m] = nums2[i]
+    nums1.sort()
     return nums1
     
 
