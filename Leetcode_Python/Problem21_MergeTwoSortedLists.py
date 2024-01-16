e0f8ac18339f7378c1ff8c43855df7b3120127bd
shimame <adcshun143@gmail.com>
2024-01-16 22:09:14 +0900

    MergeTwoLinkedLists

--- Leetcode_Python/Problem21_MergeTwoSortedLists.py
+++ Leetcode_Python/Problem21_MergeTwoSortedLists.py
@@ -1,26 +1,33 @@
-import ListNode
+from linked_list import *
+from ListNode import ListNode
 
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        dummy = ListNode.ListNode(0)
-        cur = ListNode.ListNode(0)
-        while l1 and l2:
-            if l1.val < l2.val:
-                cur.next = l1
-                l1 = l1.next
-            else:
-                cur.next = l2
-                l2 = l2.next
-            cur = cur.next
-        cur.next = l1 or l2
-        return dummy.next
 
-nums1 = [1,2,4]
-nums2 = [1,3,4]
-solution = Solution()
+def mergeTwoLists(list1, list2):
+    head = ListNode()
+    current = head
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
 
-for i in range(len(nums1)):
-    if (i == len(nums1)):
-        nodes1 = ListNode.ListNode(nums1[i], nums1[i+1])
+    current.next = list1 or list2
+    return head.next
 
-print(solution.mergeTwoLists(nodes1,nodes2))
+
+list1 = LinkedList()
+print('insert 1,2,4...')
+list1.insert(1)
+list1.insert(2)
+list1.insert(4)
+
+list2 = LinkedList()
+print('insert 1,3,4...')
+list2.insert(1)
+list2.insert(3)
+list2.insert(4)
+
+print(mergeTwoLists(list1,list2))
