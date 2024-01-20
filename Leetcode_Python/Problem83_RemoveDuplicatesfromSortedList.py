6ae5eb508ba606667f62330f842701d70932ab26
shimame <adcshun143@gmail.com>
2024-01-20 21:23:59 +0900

    DeleteDuplicatesLinkedList

---
+++ Leetcode_Python/Problem83_RemoveDuplicatesfromSortedList.py
@@ -0,0 +1,7 @@
+def deleteDuplicates(self, head):
+    current = head
+    while current:
+        while current.next and current.next.val == current.val:
+            current.next = current.next.next
+        current = current.next
+    return head
