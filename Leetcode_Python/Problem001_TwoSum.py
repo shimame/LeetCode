3d79c09803268a0626401b5ef5d15971ed3c1182
shimame <adcshun143@gmail.com>
2024-01-29 09:41:06 +0900

    ElementNumbersOfTwoSumEqual9

--- Leetcode_Python/Problem001_TwoSum.py
+++ Leetcode_Python/Problem001_TwoSum.py
@@ -1,8 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        nums = [2,7,11,15]
+        target = 9
+        answer = 0
+        for i, num1 in enumerate(nums):
+            for j, num2 in enumerate(nums):
+                answer = num1 + num2
+                if(target == answer and i < j):
+                    return i,j
+
 nums = [2,7,11,15]
 target = 9
-answer = 0
-for i, num1 in enumerate(nums):
-    for j, num2 in enumerate(nums):
-        answer = num1 + num2
-        if(target == answer and i < j):
-            print(str(i) + "," + str(j))        
+instance = Solution()
+print(instance.twoSum(nums, target))
