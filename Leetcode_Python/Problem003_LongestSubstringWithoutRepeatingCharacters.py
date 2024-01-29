48932f517d5eee03f8be30d6b684c099973f7512
shimame <adcshun143@gmail.com>
2024-01-29 09:45:30 +0900

    LongestLengthOfStringWithoutRepeating

--- Leetcode_Python/Problem003_LongestSubstringWithoutRepeatingCharacters.py
+++ Leetcode_Python/Problem003_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,18 +1,21 @@
-def lengthOfLongestSubstring(s):
-    i = 0
-    strs = list(s)
-    memory = ""
-    max_length = 0
-    
-    while i < len(strs):
-        if strs[i] in memory:
-            memory =  memory[memory.index(strs[i])+1:]
-
-        memory = memory + strs[i]
-        i += 1
-        max_length = max(max_length, len(memory))
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        i = 0
+        strs = list(s)
+        memory = ""
+        max_length = 0
         
-    return max_length
+        while i < len(strs):
+            if strs[i] in memory:
+                memory =  memory[memory.index(strs[i])+1:]
+
+            memory = memory + strs[i]
+            i += 1
+            max_length = max(max_length, len(memory))
+            
+        return max_length
+
 
 s = "abcabcbb"
-print(lengthOfLongestSubstring(s))
+instance = Solution()
+print(instance.lengthOfLongestSubstring(s))
