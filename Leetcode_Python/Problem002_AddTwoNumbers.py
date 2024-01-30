a97b441a8bdad562da61fbe4e1dbf51970968317
shimame <adcshun143@gmail.com>
2024-01-30 15:40:28 +0900

    ListNodeAddTwoNumbers

--- Leetcode_Python/Problem002_AddTwoNumbers.py
+++ Leetcode_Python/Problem002_AddTwoNumbers.py
@@ -1,17 +1,18 @@
-def addTwoNumbers(l1, l2):
-    dummy = cur = ListNode(0)
-    carry = 0
-    while l1 or l2 or carry:
-        if l1:
-            carry += l1.val
-            l1 = l1.next
-        if l2:
-            carry += l2.val
-            l2 = l2.next
-        cur.next = ListNode(carry%10)
-        cur = cur.next
-        carry = carry // 10
-    return dummy.next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry%10)
+            cur = cur.next
+            carry = carry // 10
+        return dummy.next
 
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
