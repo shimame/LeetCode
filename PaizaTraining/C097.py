c2e9a4b82e74cf63b641fe19ce2a087e5b18bbb8
shimame <adcshun143@gmail.com>
2024-06-02 23:48:17 +0900

    add gcd function

--- PaizaTraining/C097.py
+++ PaizaTraining/C097.py
@@ -1,11 +1,23 @@
-import math
+def gcd(x, y):
+    if x < y:
+        x, y = y, x
+    if x % y == 0:
+        return x
+    r = x % y
+    while True:
+        x, y = y, r
+        r = x % y
+        if r == 0:
+            break
+    return y
+
 s = input().rstrip().split(' ')
 answer = ["N"]
 n = int(s[0])
 x = int(s[1])
 y = int(s[2])
-both = math.lcm(x, y)
-print(both) #ここが直さないといけない
+both = gcd(x, y)
+# print(both) #ここが直さないといけない
 for i in range(2, n+1):      
     if i % both == 0:
         answer.append("AB")
