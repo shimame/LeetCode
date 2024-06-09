55fee41f2ad21779f32ec9e4a9ec55d162f6a46e
shimame <adcshun143@gmail.com>
2024-06-09 21:00:03 +0900

    ten outputs separated by comma version 1

---
+++ PaizaTraining/level_up_workbook/rankC/standard_output_menu/rankC_comma/step3.py
@@ -0,0 +1,9 @@
+input_line = input()
+s = []
+for i in range(len(input_line)):
+    if input_line[i] == " ":
+        s.append(",")
+    else:
+        s.append(input_line[i])
+s.append(",")
+print("".join(s))
