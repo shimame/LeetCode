e260efc9a5d7e90974ba1e03118cbbd364d0e4b7
shimame <adcshun143@gmail.com>
2024-06-25 22:36:51 +0900

    random access

---
+++ PaizaTraining/level_up_workbook/rankC/data_set_selection_menu/rankD_dynamic_array/step1.py
@@ -0,0 +1,12 @@
+input_line = input().split()
+N = input_line[0]
+M = int(input_line[1])
+A = input().split()
+print(A[M-1])
+
+"""answewr
+N, M = map(int, input().split())
+A = [int(x) for x in input().split()]
+
+print(A[M-1])
+"""
