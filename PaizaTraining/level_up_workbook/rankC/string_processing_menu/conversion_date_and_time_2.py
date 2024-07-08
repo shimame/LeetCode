641899401ea6a56b9971c409da4b00e570ea632f
shimame <adcshun143@gmail.com>
2024-07-08 15:13:41 +0900

    Conversion date and time version 2

---
+++ PaizaTraining/level_up_workbook/rankC/string_processing_menu/conversion_date_and_time_2.py
@@ -0,0 +1,11 @@
+input_line = input()
+year = input_line[:4]
+month = input_line[5:7]
+day = input_line[8:10]
+hour = input_line[11:13]
+minute = input_line[14:]
+print(year)
+print(month)
+print(day)
+print(hour)
+print(minute)
