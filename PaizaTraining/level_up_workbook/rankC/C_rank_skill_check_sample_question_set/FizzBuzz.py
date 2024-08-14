ea1f84b665ae8623c820b17ae88a638bb66e781d
shimame <adcshun143@gmail.com>
2024-08-14 23:30:13 +0900

    Fizz Buzz

---
+++ PaizaTraining/level_up_workbook/rankC/C_rank_skill_check_sample_question_set/FizzBuzz.py
@@ -0,0 +1,10 @@
+N = int(input())
+for i in range(1, N+1):
+    if i % 15 == 0:
+        print("Fizz Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
