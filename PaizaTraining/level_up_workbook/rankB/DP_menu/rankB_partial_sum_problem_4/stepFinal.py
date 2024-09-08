ee37fca3d73445b0877a872615040d26b3c92b6c
shimame <adcshun143@gmail.com>
2024-09-08 18:35:09 +0900

    Partial sum problem version 4

---
+++ PaizaTraining/level_up_workbook/rankB/DP_menu/rankB_partial_sum_problem_4/stepFinal.py
@@ -0,0 +1,12 @@
+n, x = [int(x) for x in input().split()]
+a = [int(input()) for _ in range(n)]
+dp = [False] * (x+1)
+dp[0] = True
+for i in a:
+    for j in range(i, x+1):
+        if dp[j - i]:
+            dp[j] = True
+if dp[x]:
+    print("yes")
+else:
+    print("no")
