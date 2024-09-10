e01d520ebefaacc6b6c447e4ab1b912d0ad1a3ef
shimame <adcshun143@gmail.com>
2024-09-10 17:38:10 +0900

    Fix s list

--- PaizaTraining/level_up_workbook/rankB/new_B_rank_level_up_menu/rewriting_map_1_mass.py
+++ PaizaTraining/level_up_workbook/rankB/new_B_rank_level_up_menu/rewriting_map_1_mass.py
@@ -1,13 +1,9 @@
 H, W = [int(x) for x in input().split()]
-S = list([input() for _ in range(H)])
+S = [list(input()) for _ in range(H)]
 y, x = [int(x) for x in input().split()]
 if S[y][x] == ".":
-    S[y] = list(S[y])
     S[y][x] = "#"
-    S[y] = "".join(S[y])
 elif S[y][x] == "#":
-    S[y] = list(S[y])
     S[y][x] = "."
-    S[y] = "".join(S[y])
 for i in range(H):
-    print(S[i])
+    print("".join(S[i]))
