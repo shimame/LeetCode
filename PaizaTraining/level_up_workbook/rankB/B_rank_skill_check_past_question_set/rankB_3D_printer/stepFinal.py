f27b4886fdd560401c0da6b3ef5f3cb8c967a648
shimame <adcshun143@gmail.com>
2024-10-15 23:30:10 +0900

    Fix error in progress

---
+++ PaizaTraining/level_up_workbook/rankB/B_rank_skill_check_past_question_set/rankB_3D_printer/stepFinal.py
@@ -0,0 +1,29 @@
+X, Y, Z = [int(i) for i in input().split()]
+d = []
+ans = []
+for _ in range(Z):
+    data = []
+    for _ in range(X):
+        row = input()
+        data.append(row)
+    input()
+    d.append(data)
+t_d = list(zip(*d))
+print(t_d)
+for i in range(Z):
+    ans_row = []
+    for j in range(Y):
+        row = t_d[j][i]
+        print(f"{i},{j} row={row}")
+        if row == "." * X:
+            ans_row.append(".")
+        else:
+            ans_row.append("#")
+    ans.append("".join(ans_row))
+print(ans)
+for k in reversed(ans):
+    print(k)
+"""
+for j in range(0, Z, Y):
+    print(:j)
+"""
