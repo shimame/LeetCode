2bc8d6eb4050f5933eaa30f352745d6f7e25813d
shimame <adcshun143@gmail.com>
2024-11-09 10:52:56 +0900

    Taking one mass version 2

---
+++ PaizaTraining/level_up_workbook/rankA/A_rank_level_up_menu/rankA_domination_game/step2.py
@@ -0,0 +1,21 @@
+H, W = [int(x) for x in input().split()]
+S_map = []
+for i in range(H):
+    input_line = list(input())
+    if "*" in input_line:
+        now_posi = (i, input_line.index("*"))
+    S_map.append(input_line)
+
+y = now_posi[0]
+x = now_posi[1]
+if 0 < y and S_map[y-1][x] != "#":
+    S_map[y-1][x] = "*"
+if y < H - 1 and S_map[y+1][x] != "#":
+    S_map[y+1][x] = "*"
+if 0 < x and S_map[y][x-1] != "#":
+    S_map[y][x-1] = "*"
+if x < W - 1 and S_map[y][x+1] != "#":
+    S_map[y][x+1] = "*"
+
+for j in range(H):
+    print("".join(S_map[j]))
