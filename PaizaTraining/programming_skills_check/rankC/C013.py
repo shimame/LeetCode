3709321fc786dc056f2a83e2e856b6a0273da432
shimame <adcshun143@gmail.com>
2025-01-30 19:10:49 +0900

    Disliked number

---
+++ PaizaTraining/programming_skills_check/rankC/C013.py
@@ -0,0 +1,12 @@
+unlike_num = input()
+num = int(input())
+ans = []
+for _ in range(num):
+    room_num = input()
+    if unlike_num not in room_num:
+        ans.append(room_num)
+
+if len(ans) == 0:
+    print("none")
+else:
+    print("\n".join(ans))
