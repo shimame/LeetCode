b37ae8d72a5ec5d6c09837feb4b37971a0ffd5e5
shimame <adcshun143@gmail.com>
2025-05-08 20:31:05 +0900

    Fax paper collection

---
+++ PaizaTraining/programming_skills_check/rankC/C025.py
@@ -0,0 +1,19 @@
+m = int(input())
+n = int(input())
+fax = [list(map(int, input().split())) for _ in range(n)]
+amount = fax[0][2]
+count = 0
+for i in range(1, n):
+    if fax[i][0] > fax[i-1][0]:
+        count += amount // m
+        if amount % m != 0:
+            count += 1
+        amount = fax[i][2]
+    else:
+        amount += fax[i][2]
+    print(i, amount, count)
+count += amount // m
+if amount % m != 0:
+    count += 1
+
+print(count)
